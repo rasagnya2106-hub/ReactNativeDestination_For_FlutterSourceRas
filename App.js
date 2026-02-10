deb4b1c45a65c53998a0b29b8d09f4edc38dcd90
rasagnya2106-hub <rasagnya2106@gmail.com>
2026-02-10 16:38:52 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function App() {
+  return (
+    <View>
+      <Text>Flutter to React Native Migration Test</Text>
+    </View>
+  );
+}
